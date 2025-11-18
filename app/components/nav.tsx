a035505ef119cf44ae68266424840fd928b98361
Reynaldi Chernando <reynaldichernando@gmail.com>
2025-11-18 13:17:37 +0700

    Make nav sticky

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -4,8 +4,8 @@ import { UserNav } from "./user-nav";
 
 export default function Nav() {
   return (
-    <div className="border-b">
-      <div className="flex h-16 items-center px-4">
+    <div className="h-16">
+      <div className="h-16 border-b fixed bg-background w-full flex items-center px-4">
         <Logo className="w-8 min-w-8 drop-shadow-md" />
         <MainNav className="mx-6" />
         <div className="ml-auto flex items-center space-x-4">
