24987c6a9fbaa43204cf7ec56dc5d61efd196962
Reynaldi Chernando <reynaldichernando@gmail.com>
2026-02-05 16:04:03 +0700

    Move email verify to auth page

--- app/app/auth/page.tsx
+++ app/app/auth/page.tsx
@@ -3,6 +3,7 @@ import { Logo } from "@/components/Logo";
 import { IS_CLOUD, DISABLE_SIGNUP } from "@/config/constants";
 import { Metadata } from "next";
 import { AuthForm } from "@/components/auth-form";
+import { EmailVerifyForm } from "@/components/email-verify-form";
 import { TestimonialCarousel } from "@/components/TestimonialCarousel";
 
 export const dynamic = "force-dynamic";
@@ -11,19 +12,30 @@ export const metadata: Metadata = {
   title: "Authentication | Corsfix Dashboard",
 };
 
-export default function AuthenticationPage() {
+export default async function AuthenticationPage({
+  searchParams,
+}: {
+  searchParams: Promise<{ emailVerify?: string }>;
+}) {
+  const { emailVerify } = await searchParams;
   return (
     <>
       <div className="px-4 relative h-screen flex-col items-center justify-center grid lg:max-w-none lg:grid-cols-2 lg:px-0">
-        <Link href="https://corsfix.com" className="absolute top-6 left-6 flex items-center lg:hidden">
+        <Link
+          href="https://corsfix.com"
+          className="absolute top-6 left-6 flex items-center lg:hidden"
+        >
           <Logo className="mr-2 size-8" />
           <span className="text-lg font-medium">Corsfix</span>
         </Link>
         <div className="relative hidden h-full flex-col p-16 text-white lg:flex">
           <div className="inset-0 absolute w-full h-full p-5">
-            <img src="/mesh3.svg" className="h-full w-full object-cover rounded-2xl" />
+            <img
+              src="/mesh3.svg"
+              className="h-full w-full object-cover rounded-2xl"
+            />
           </div>
-          <div className="absolute inset-5 bg-gradient-to-t from-black/60 via-transparent to-transparent rounded-2xl" />
+          <div className="absolute inset-5 bg-gradient-to-t from-black/10 via-transparent to-transparent rounded-2xl" />
           <div className="relative z-20 flex items-center text-lg font-medium">
             <Link href="https://corsfix.com" className="flex items-center">
               <Logo className="mr-2 size-8" />
@@ -35,7 +47,11 @@ export default function AuthenticationPage() {
           </div>
         </div>
         <div className="lg:p-8">
-          <AuthForm isCloud={IS_CLOUD} disableSignup={DISABLE_SIGNUP} />
+          {emailVerify ? (
+            <EmailVerifyForm encodedPath={emailVerify} />
+          ) : (
+            <AuthForm isCloud={IS_CLOUD} disableSignup={DISABLE_SIGNUP} />
+          )}
         </div>
       </div>
     </>
